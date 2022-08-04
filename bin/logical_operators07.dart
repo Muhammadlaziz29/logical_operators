6b27ac9fb42af2a3c26572b3e6a505ed5c5ff040
Muhammadlaziz29 <muxammadlaziz29@gmail.com>
2022-08-04 22:31:54 +0500

    hello

--- bin/logical_operators07.dart
+++ bin/logical_operators07.dart
@@ -9,4 +9,11 @@ Given two integers a, b,  check the following statement "At least one  of the nu
         bool: answer
 */
 
-void main() {}
+
+bool func(int a, int b) {
+    return a<0 || b<0;
+}
+
+void main() {
+    print(func(-1,2));
+}
